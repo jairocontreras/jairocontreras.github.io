820e1865079d64ed1e6193ee7bb6220e3d0aa0c9
jairocontreras <43326330+jairocontreras@users.noreply.github.com>
2024-07-13 17:36:11 -0700

    update

--- script/ahk.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hidden = document.getElementById("hidden");
-  document.getElementById("startup").addEventListener("click", function() {
-    this.classList.toggle("collapse");
-    hidden.style.height = (!hidden.offsetHeight ? hidden.scrollHeight + "px" : 0);
-  });
-});
--- script/pass.js
+++ script/pass.js
@@ -1,41 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz",
+  const abc = "abcdefghijklmnopqrstuvwxyz",
         number = document.getElementById("number"),
         generate = document.getElementById("generate"),
         password = document.getElementById("password"),
         copy = document.getElementById("copy"),
         clear = document.getElementById("clear");
-  let options = "";
+  var options = "";
 
-  for (let i = 1; i < 27; i++) {
+  for (let i = 1; i < 27; i++)
     options += "<option>" + i;
-  }
-  number.innerHTML += options;
+  number.innerHTML = options;
 
   generate.addEventListener("click", () => {
-    const length = number.value,
-          buffer_len = length-1;
-    let buffer = "",
-        charset = alphabet;
+    const buffer_len = number.value-1;
+    var buffer = "",
+        charset = abc;
     for (let i = 0; i < buffer_len; i++) {
       while (true) {
         let pos = Math.floor(Math.random() * charset.length),
-            strNewTmp = charset.substring(pos, pos+1),
-            tmp = buffer.indexOf(strNewTmp);
-        if (tmp == -1) {
-          buffer += strNewTmp;
+            str_new_tmp = charset.substring(pos, pos+1),
+            tmp = buffer.indexOf(str_new_tmp);
+        if (tmp === -1) {
+          buffer += str_new_tmp;
           break
         }
         else
-          charset = charset.replace(strNewTmp, "");
+          charset = charset.replace(str_new_tmp, "");
       }
     }
-    charset = alphabet;
+    charset = abc;
     while (true) {
       let pos = Math.floor(Math.random() * charset.length),
           new_tmp = charset.substring(pos, pos+1),
           tmp = buffer.indexOf(new_tmp);
-      if (tmp == -1) {
+      if (tmp === -1) {
         const insert_pos = Math.floor(Math.random() * buffer_len);
         var result = buffer.substring(0, insert_pos) + new_tmp + buffer.substring(insert_pos, buffer_len);
         break
@@ -46,18 +44,16 @@ document.addEventListener("DOMContentLoaded", () => {
     password.value = result;
   });
 
-  copy.addEventListener("click", function() {
+  copy.addEventListener("click", () => {
     navigator.clipboard.writeText(password.value);
-    this.innerText = "Copied!";
-    this.classList.add("copied");
+    copy.innerText = "Copied!";
+    copy.classList.add("copied");
   });
 
-  copy.addEventListener("blur", function() {
-    this.innerText = "Copy";
-    this.classList.remove("copied");
+  copy.addEventListener("blur", () => {
+    copy.innerText = "Copy";
+    copy.classList.remove("copied");
   });
 
-  clear.addEventListener("click", () => {
-    password.value = "";
-  });
+  clear.addEventListener("click", () => password.value = "");
 });
